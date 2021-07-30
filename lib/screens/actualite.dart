99b8c61fb6232831894f3646995ecfea176ba634
toubal77 <72252709+toubal77@users.noreply.github.com>
2021-07-30 21:37:30 +0200

    add story

--- lib/screens/actualite.dart
+++ lib/screens/actualite.dart
@@ -1,6 +1,36 @@
 import 'package:flutter/material.dart';
 
 class Actualits extends StatelessWidget {
+  List _store = [
+    {
+      'image': 'assets/images/évent 1.png',
+      'nom': 'Aventure oran',
+    },
+    {
+      'image': 'assets/images/évent 2.png',
+      'nom': 'AlphaRando',
+    },
+    {
+      'image': 'assets/images/évent 3.png',
+      'nom': 'Orandonlina',
+    },
+    {
+      'image': 'assets/images/évent 4.png',
+      'nom': 'Oran Travel',
+    },
+    {
+      'image': 'assets/images/évent5.png',
+      'nom': 'Fox je sais pas',
+    },
+    {
+      'image': 'assets/images/évent 1.png',
+      'nom': 'Aventure oran',
+    },
+    {
+      'image': 'assets/images/évent 2.png',
+      'nom': 'AlphaRando',
+    }
+  ];
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -63,6 +93,64 @@ class Actualits extends StatelessWidget {
                 ),
               ),
             ),
+            Container(
+              padding: const EdgeInsets.only(top: 9.0, left: 21.0),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'recent stories ...',
+                style: TextStyle(
+                  fontFamily: 'Lato-Light',
+                  color: Color.fromRGBO(0, 0, 0, 0.58),
+                  fontSize: 12,
+                ),
+              ),
+            ),
+            Container(
+              height: 102,
+              padding: const EdgeInsets.only(top: 10, left: 11),
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: _store.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 64,
+                          width: 64,
+                          margin: const EdgeInsets.only(right: 5),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(37),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color(0xFF334D73),
+                                spreadRadius: 0,
+                                blurRadius: 4,
+                                offset: Offset(0, 3),
+                              ),
+                            ],
+                          ),
+                          child: Container(
+                            child: Image.asset(_store[index]['image']),
+                          ),
+                        ),
+                        Container(
+                          height: 18,
+                          width: 53,
+                          child: Flexible(
+                            child: Text(
+                              _store[index]['nom'],
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            )
           ],
         ),
       ),
