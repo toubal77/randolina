dd355ef04a07d71d5798bc1e64c68f8f7220363f
toubal77 <72252709+toubal77@users.noreply.github.com>
2021-08-01 17:34:52 +0200

    recent event profile agence

--- lib/screens/profile_agence/agence_profile.dart
+++ lib/screens/profile_agence/agence_profile.dart
@@ -12,6 +12,37 @@ class AgenceProfile extends StatelessWidget {
         child: Column(
           children: [
             HeaderAgence(),
+            Container(
+              margin: const EdgeInsets.only(top: 15, left: 23),
+              height: 193,
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(top: 9.0, left: 27.0),
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'recent events ...',
+                      style: TextStyle(
+                        // fontFamily: 'Lato-Black',
+                        color: Color.fromRGBO(0, 0, 0, 0.58),
+                        fontWeight: FontWeight.w600,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 7),
+                    width: 313,
+                    height: 163,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/Rectangle 5.png'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
